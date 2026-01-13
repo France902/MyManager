bb9b90b21dfd7a19e4f06ee894cc8f174d038745
France <frascic231@gmail.com>
2026-01-14 00:50:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,31 +8,103 @@ function stampaData() {
 stampaData();
 
 
-function spostaCalendario(direction) {
-   
-    const contenitore = document.getElementById('griglia');
+function generaGrigliaOraria() {
+    const container = document.getElementById('griglia');
+    
+    
+    
+    container.innerHTML = '';
 
-    const arrayDiDiv = [...contenitore.children];
+    for (let ora = 0; ora < 24; ora++) {
+        
+        const stringaOra = ora.toString().padStart(2, '0') + ':00';
 
-    arrayDiDiv.forEach(div => {
-        div.setAttribute('tabindex', '0'); 
-        div.setAttribute('role', 'button'); 
-    });
+        
+        const cellaOra = document.createElement('div');
+        cellaOra.className = 'grid-item';
+        cellaOra.style.height = '50px';
+        cellaOra.style.border = 'none';
+        cellaOra.style.background = 'none';
+        cellaOra.textContent = stringaOra;
+        container.appendChild(cellaOra);
 
-    if(direction == "right") dataCalendario.setMonth(dataCalendario.getMonth() + 1);
-    else dataCalendario.setMonth(dataCalendario.getMonth() - 1);
+        
+        for (let giorno = 0; giorno < 7; giorno++) {
+            const cellaGiorno = document.createElement('div');
+            cellaGiorno.className = 'grid-item';
+            
+            cellaGiorno.style.height = '50px'; 
+            container.appendChild(cellaGiorno);
+        }
+    }
+    generaGrigliaGiorni();
+}
 
-    stampaData();
+function generaGrigliaGiorni() {
+    const containerGiorni = document.getElementById('griglia_giorni');
+    const arrayGiorni = [...containerGiorni.children];
 
-    if(dataCalendario.getMonth() == 3 || dataCalendario.getMonth() == 5 || dataCalendario.getMonth() == 8 || dataCalendario.getMonth() == 10) {
-        arrayDiDiv[30].style.display = 'none';
-    } else if(dataCalendario.getMonth() == 1) {
-        arrayDiDiv[28].style.display = 'none';
-        arrayDiDiv[29].style.display = 'none';
-        arrayDiDiv[30].style.display = 'none';
-    } else {
-        arrayDiDiv[28].style.display = 'block';
-        arrayDiDiv[29].style.display = 'block';
-        arrayDiDiv[30].style.display = 'block';
+    for(let i=dataCalendario.getDay();i<7;i++) {
+        switch(i) {
+            case 0:
+                arrayGiorni[i].innerHTML = "Dom";
+                break;
+            case 1:
+                arrayGiorni[i].innerHTML = "Lun";
+                break;
+            case 2:
+                arrayGiorni[i].innerHTML = "Mar";
+                break;
+            case 3:
+                arrayGiorni[i].innerHTML = "Mer";
+                break;
+            case 4:
+                arrayGiorni[i].innerHTML = "Gio";
+                break;
+            case 5:
+                arrayGiorni[i].innerHTML = "Ven";
+                break;
+            case 6:
+                arrayGiorni[i].innerHTML = "Sab";
+                break;
+        }
+        arrayGiorni[i].innerHTML += "<br>" + (dataCalendario.getDate() + (i-dataCalendario.getDay()));
+    }
+
+    for(let i=dataCalendario.getDay();i>-1;i--) {
+        switch(i) {
+            case 0:
+                arrayGiorni[i].innerHTML = "Dom";
+                break;
+            case 1:
+                arrayGiorni[i].innerHTML = "Lun";
+                break;
+            case 2:
+                arrayGiorni[i].innerHTML = "Mar";
+                break;
+            case 3:
+                arrayGiorni[i].innerHTML = "Mer";
+                break;
+            case 4:
+                arrayGiorni[i].innerHTML = "Gio";
+                break;
+            case 5:
+                arrayGiorni[i].innerHTML = "Ven";
+                break;
+            case 6:
+                arrayGiorni[i].innerHTML = "Sab";
+                break;
+        }
+        arrayGiorni[i].innerHTML += "<br>" + (dataCalendario.getDate() + (i-dataCalendario.getDay()));
     }
 }
+
+function spostaCalendario(direction) {
+    if(direction == "right") dataCalendario.setDate(dataCalendario.getDate() + 7);
+    else dataCalendario.setDate(dataCalendario.getDate() - 7);
+    stampaData();
+    generaGrigliaGiorni();
+}
+
+
+window.onload = generaGrigliaOraria;
